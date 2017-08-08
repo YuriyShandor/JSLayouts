6c066cffda78e5768c03f9c5958d5eb709abdc48
YuriyShandor <yuriyshandor@gmail.com>
2017-08-08 18:48:02 +0300

    Change task_12

--- task_12/js/script.js
+++ task_12/js/script.js
@@ -1,7 +1,11 @@
 var divs = document.getElementsByTagName("div");
 
+var span = document.createElement('span');
+
+
 for (var i = 0; i < divs.length; i++) {
   divs[i].onclick = function () {
-    console.log("Ви клікнули на " + divs[i].textContent + " населення становить: " + divs[i].getAttribute('value') + " осіб");
+    span.innerHTML = " Ви клікнули на " + this.textContent + ". Населення -- " + this.getAttribute('value') + " осіб";
+    this.appendChild(span);
   };
 }
