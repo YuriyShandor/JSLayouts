baff70f6e7a648c99820b616544035c2f447cc88
Yuriy Shandor <yuriyshandor@gmail.com>
2018-07-04 12:48:56 +0300

    Add Sorting by Tab. Mod_02

--- js/sorting.js
+++ js/sorting.js
@@ -11,21 +11,39 @@ $(document).ready(function () {
 
   // Sorting by classes
 
-  var colorTabArr = ['all', 'red', 'black', 'green', 'blue', 'grey'];
+  var colorSortTabArr = ['all', 'red', 'black', 'green', 'blue', 'grey'];
 
   $('.tab-sorting__tab_all').addClass('active');
 
-  colorTabArr.forEach(function (item) {
+  colorSortTabArr.forEach(function (item) {
     $('.tab-sorting__tab_' + item).click(function () {
-      $('.tab-sorting__tab').removeClass('active');
+      $(this).closest(".tab-sorting__block_classes").find('.tab-sorting__tab').removeClass('active');
       $(this).addClass('active');
-      $('.tab-sorting__element').hide();
-      $('.tab-sorting__element_' + item).show();
+      $(this).closest(".tab-sorting__block_classes").find('.tab-sorting__element').hide();
+      $(this).closest(".tab-sorting__block_classes").find('.tab-sorting__element_' + item).show();
     });
   });
+
   $('.tab-sorting__tab_all').click(function () {
-    $('.tab-sorting__tab').removeClass('active');
+    $(this).closest(".tab-sorting__block_classes").find('.tab-sorting__tab').removeClass('active');
     $(this).addClass('active');
-    $('.tab-sorting__element').show();
+    $(this).closest(".tab-sorting__block_classes").find('.tab-sorting__element').show();
+  });
+
+  // Sorting by data attribute
+  $('.tab-sorting__tab[data-sorting-tab-title="all"]').addClass('active');
+
+  $('.tab-sorting__tab').click(function () {
+    $(this).closest(".tab-sorting__block_data").find('.tab-sorting__tab').removeClass('active');
+    $(this).addClass('active');
+    var colorSortTabId = $(this).data("sorting-tab-title");
+    $(this).closest(".tab-sorting__block_data").find(".tab-sorting__element").hide();
+    $(".tab-sorting__element[data-sorting-tab-element='" + colorSortTabId + "']").show();
+  });
+
+  $('.tab-sorting__tab[data-sorting-tab-title="all"]').click(function () {
+    $(this).closest(".tab-sorting__block_data").find('.tab-sorting__tab').removeClass('active');
+    $(this).addClass('active');
+    $(this).closest(".tab-sorting__block_data").find('.tab-sorting__element').show();
   });
 });
