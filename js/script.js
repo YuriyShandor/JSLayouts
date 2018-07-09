899852f86142bcd6f1b87a2d12ebaacdf7fa678f
Yuriy Shandor <yuriyshandor@gmail.com>
2018-07-09 18:42:40 +0300

    Add Sum Of Items Values

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 'use strict';
 
 $(document).ready(function () {
-  // ============ Main nav-bar =============
+  // ================= Main nav-bar =====================
   $('.toggler-icon').on('click', function () {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  // ================= Sum of items values ===============
+
+  $('.sumvalitems-block__item').click(function () {
+    $(this).toggleClass('active');
+    culcSumValItems();
+  });
+
+  var sumValItemsArr = $('.sumvalitems-block__item');
+
+  function culcSumValItems() {
+    var sumValItemsVal = 0;
+
+    for (var i = 0; i < sumValItemsArr.length; i++) {
+      if (sumValItemsArr[i].classList.contains('active')) {
+        sumValItemsVal += parseInt(sumValItemsArr[i].getAttribute('data-sumvalitems'));
+      };
+    };
+
+    $('.sumvalitems-block__value').html(sumValItemsVal);
+  };
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-  // ============ Main nav-bar =============
+  // ================= Main nav-bar =====================
   $('.toggler-icon').on('click', function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
 
-  
+  // ================= Sum of items values ===============
+
+  $('.sumvalitems-block__item').click(function() {
+    $(this).toggleClass('active');
+    culcSumValItems();
+  });
+
+  let sumValItemsArr = $('.sumvalitems-block__item');
+
+  function culcSumValItems() {
+    let sumValItemsVal = 0;
+
+    for(let i =0; i < sumValItemsArr.length; i++) {
+      if(sumValItemsArr[i].classList.contains('active')) {
+        sumValItemsVal += parseInt(sumValItemsArr[i].getAttribute('data-sumvalitems'));
+      };
+    };
+
+    $('.sumvalitems-block__value').html(sumValItemsVal);
+  };
+
+
 });
